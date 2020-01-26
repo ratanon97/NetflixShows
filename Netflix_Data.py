c2148fed285d0a7158f1e0a65fdd667467367dbf
Tap Suksumrun <ratanon97@users.noreply.github.com>
2020-01-26 14:22:06 +0000

    Factorised show ID and release years

--- Netflix_Data.py
+++ Netflix_Data.py
@@ -15,7 +15,11 @@ netflix_shows_2020  = pd.read_csv("netflix_titles.csv")
 #-------------------------------------------------------------------------------
 #First task
 #Understanding what content is available in different countries
-netflix_shows_2020=netflix_shows_2020.dropna(subset=["country"]) #Drop na values in the column: country
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):  
     print(netflix_shows_2020)
-#Specify which rows and columns the dataframe should be showw
+#Specify which rows and columns the dataframe should be show#Specify which rows and columns the dataframe should be show
+netflix_shows_2020=netflix_shows_2020.dropna(subset=["country"]) #Drop na values in the column: country
+netflix_shows_2020.info()
+netflix_shows_2020["show_id"] = pd.Categorical(netflix_shows_2020["show_id"])
+netflix_shows_2020["release_year"] = pd.Categorical(netflix_shows_2020["release_year"])
+
