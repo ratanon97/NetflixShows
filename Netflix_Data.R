7e595abcd56024abce22f5269ee8120c76de3110
Tap Suksumrun <ratanon97@users.noreply.github.com>
2020-01-26 12:11:22 +0000

    Implemented minimal pie chart of the TV shows and movies in America

--- Netflix_Data.R
+++ Netflix_Data.R
@@ -8,6 +8,7 @@ getwd()
 setwd("C:\\Users\\KOMSUN\\Documents\\Kaggle\\Netflix Shows")
 library(tidyverse)
 netflix_shows_2020 <- read_csv('netflix_titles.csv') #Note: read_csv is part of the tidyverse
+options(max.print=1000000) #Increasing printing size when executing a dataframe in the console
 #-------------------------------------------------------------------------------
 #First task
 #Understanding what content is available in different countries
@@ -15,20 +16,33 @@ netflix_shows_2020 <- read_csv('netflix_titles.csv') #Note: read_csv is part of 
 #Data Frame Manipulation
 #Filter out shows with no country data
 netflix_shows_2020_backup <- netflix_shows_2020 #Create backup of the dataset
-options(max.print=1000000) #Increasing printing size when executing a dataframe in the console
 #Check rows with empty values
 netflix_shows_2020[!complete.cases(netflix_shows_2020),] #Check to see which rows have empty values
 netflix_shows_2020 <- netflix_shows_2020[!is.na(netflix_shows_2020$country),] #Remove shows with no country data
 #Factorise show IDs and Release years
 netflix_shows_2020$show_id <- factor(netflix_shows_2020$show_id)
 netflix_shows_2020$release_year <- factor(netflix_shows_2020$release_year)
-
 #Shows in USA
 library(dplyr) #To make it fail-safe if tidyverse package was not imported
 USA_netflix_shows_2020 <- netflix_shows_2020 %>%
   filter(str_detect(country,"United States")) #Filter Countries with the pattern United States in the column
-#Visualise (ratings and country) using geom_count 
-library(ggplot2) #To make it fail-safe if tidyverse package was not imported
-Rating_Country <- ggplot(data=netflix_shows_2020)
-#Use the geom_count
-Rating_Country + geom_count(aes(x=country,y=rating))
+#Create pie chart to see the number of movies and TV shows are provided in the USA
+library(ggplot2) #Make it fail safe if tidyverse package was not imported
+library(scales) #For formatting the pie chart with % laabels
+Pie_Chart <- ggplot(data=USA_netflix_shows_2020,aes(x=factor(1),fill=type))+
+  geom_bar(width = 1)+
+  coord_polar("y")
+blank_theme <- theme_minimal()+
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid=element_blank(),
+    axis.ticks = element_blank(),
+    plot.title=element_text(size=14, face="bold")
+  )
+Pie_Chart + scale_fill_brewer("Blues") + blank_theme +
+  theme(axis.text.x=element_blank())+
+  geom_text(aes(y = value/2 + c(0, cumsum(value)[-length(value)]), 
+                label = percent(value/100)), size=5)
+count()
