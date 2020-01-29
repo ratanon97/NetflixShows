f7717fc85a5fcf8e78c4cc24f6e1cb035abdb56b
Tap Suksumrun <ratanon97@users.noreply.github.com>
2020-01-29 19:01:20 +0000

    Implemented Bar Chart for type of TV Shows in America

--- Netflix_Data.R
+++ Netflix_Data.R
@@ -28,7 +28,8 @@ USA_netflix_shows_2020 <- netflix_shows_2020 %>%
   filter(str_detect(country,"United States")) #Filter Countries with the pattern United States in the column
 #Create pie chart to see the number of movies and TV shows are provided in the USA
 library(ggplot2) #Make it fail safe if tidyverse package was not imported
-library(scales) #For formatting the pie chart with % laabels
+library(scales) #For formatting the pie chart with % labels
+#Pie Chart
 Pie_Chart <- ggplot(data=USA_netflix_shows_2020,aes(x=factor(1),fill=type))+
   geom_bar(width = 1)+
   coord_polar("y")
@@ -43,3 +44,18 @@ blank_theme <- theme_minimal()+
   )
 Pie_Chart + scale_fill_brewer("Blues") + blank_theme +
   theme(axis.text.x=element_blank())
+#Bar Chart
+Bar_Chart <- ggplot(data=USA_netflix_shows_2020,aes(x=type)) + 
+  geom_bar(fill="LightBlue",
+                 colour="Black",
+                 size=1) +
+  xlab("Type of Netflix Shows")+
+  ylab("Population")+
+  ggtitle("Type of TV shows provided in the USA")+ #Making the graph look nicer using the Theme function
+  theme(axis.title.x=element_text(size=15),
+        axis.title.y=element_text(size=15),
+        axis.text.x=element_text(size=15,colour="black"),
+        axis.text.y=element_text(size=15,colour="black"),
+        plot.title=element_text(size=20),
+        strip.text=element_text(size=10))
+str(USA_netflix_shows_2020$listed_in)
