a0afbf7d25e22e566cbcd0846069e998c7139541
Tap Suksumrun <ratanon97@users.noreply.github.com>
2020-01-29 20:47:57 +0000

    Plotted bar chart (count plot) on Python file

--- Netflix_Data.py
+++ Netflix_Data.py
@@ -5,6 +5,7 @@
 #Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import os
 import re
@@ -23,5 +24,14 @@ netflix_shows_2020=netflix_shows_2020.dropna(subset=["country"]) #Drop na values
 netflix_shows_2020.info()
 netflix_shows_2020["show_id"] = pd.Categorical(netflix_shows_2020["show_id"])
 netflix_shows_2020["release_year"] = pd.Categorical(netflix_shows_2020["release_year"]) #Convert data types to categorical
-USA_filter = netflix_shows_2020["country"].str.contains('United States') #Filterto create USA dataframe
+USA_filter = netflix_shows_2020["country"].str.contains('United States') #Filter to create USA dataframe by detecting columns with United States string
 USA_netflix_shows_2020 = netflix_shows_2020[USA_filter]
+#Bar Chart - to visualise the amount of TV shows and movies provided on Netflix
+sns.set_style("darkgrid")
+fig, ax = plt.subplots() #Put the whole graph in a "subplot" but will still give a normal graph #HACK
+fig.set_size_inches(11.7,8.27)
+sns.catplot(x="type", kind="count", palette="ch:.25", data=USA_netflix_shows_2020) #Create a count plot
+plt.title('Type of TV shows provided in the USA', fontsize = 16)
+plt.xlabel('Type of Netflix Shows',fontsize = 10)
+plt.ylabel('Population',fontsize = 10)
+plt.show()
